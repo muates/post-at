ce7aeb28e18dd42c8c50d993085998710a8941c4
Murat Ates <mrtt.atss@gmail.com>
2024-05-27 20:44:01 +0300

    ...

--- identity-service/src/main/java/com/muates/identityservice/service/JwtService.java
+++ identity-service/src/main/java/com/muates/identityservice/service/JwtService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
