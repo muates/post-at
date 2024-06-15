99272692a7f9a39dc0202e5827db8e1aa9b39238
Murat Ates <mrtt.atss@gmail.com>
2024-06-15 21:14:10 +0300

    schema added

---
+++ post-service/src/main/resources/init-schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS post_media (
+    id SERIAL PRIMARY KEY,
+    post_id INT NOT NULL REFERENCES posts(id),
+    media_url VARCHAR(255) NOT NULL,
+    media_type VARCHAR(50) NOT NULL CHECK (media_type IN ('image', 'video')),
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS post_likes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL REFERENCES posts(id),
+    is_like BOOLEAN NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (user_id, post_id)
+);
+
+CREATE TABLE IF NOT EXISTS post_comments (
+    id SERIAL PRIMARY KEY,
+    post_id INT NOT NULL REFERENCES posts(id),
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS post_comment_likes (
+    id SERIAL PRIMARY KEY,
+    comment_id INT NOT NULL REFERENCES post_comments(id),
+    user_id INT NOT NULL,
+    is_like BOOLEAN NOT NULL,
+    created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (comment_id, user_id)
+);
