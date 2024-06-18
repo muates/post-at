19ef477f2989035dd8e3153cc6c7e5695eaebfb7
Murat Ates <mrtt.atss@gmail.com>
2024-06-18 17:34:54 +0300

    refactoring

--- post-service/src/main/java/com/muates/postservice/controller/PostReactionController.java
+++ post-service/src/main/java/com/muates/postservice/controller/PostReactionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/post-reaction")
+@RequestMapping("/api/post/post-reaction")
 @RequiredArgsConstructor
 public class PostReactionController {
 
