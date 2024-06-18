6116b503ac1b4d3020fa51b8554aeaf7d9cd1557
Murat Ates <mrtt.atss@gmail.com>
2024-06-18 21:29:13 +0300

    ...

--- post-service/src/main/java/com/muates/postservice/repository/PostCommentLikeRepository.java
+++ post-service/src/main/java/com/muates/postservice/repository/PostCommentLikeRepository.java
@@ -4,6 +4,9 @@ import com.muates.postservice.model.entity.PostCommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostCommentLikeRepository extends JpaRepository<PostCommentLike, Long> {
+    Optional<PostCommentLike> findByUserIdAndCommentId(Long userId, Long commentId);
 }
