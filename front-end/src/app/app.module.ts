3f2875ec5991be92143ba56ebd2ec5d7cb6e348d
Murat Ates <mrtt.atss@gmail.com>
2024-07-14 23:20:10 +0300

    ...

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptor } from './core/interceptors/identity/auth-interceptor';
+import { LoginComponent } from './shared/components/identity/login/login.component';
+import { AuthService } from './core/services/identity/auth.service';
+import { AuthGuard } from './core/guards/identity/auth-guard';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './shared/components/identity/register/register.component';
+import { MainComponent } from './shared/components/main/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
