0438268516fdaa84f52bbeca4ac12cab03dd0f50
Murat Ates <mrtt.atss@gmail.com>
2024-07-14 23:19:14 +0300

    main componenet added

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './shared/components/identity/login/login.component';
+import { MainComponent } from './shared/components/main/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
